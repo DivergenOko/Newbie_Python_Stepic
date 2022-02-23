1ece694c197a78f71448d3e8cf6825196f196ef9
Yana_Senchenko <77746850+DivergenOko@users.noreply.github.com>
2022-02-23 17:43:02 +0300

    Add files via upload

---
+++ Argument_sep_end.py
@@ -0,0 +1,13 @@
+# Task №1
+print('I', 'like', 'Python', sep = '***')
+
+# Task №2
+n = input()
+k = input()
+l = input()
+m = input()
+print(k, l, m, sep = n)
+
+# Task №3
+name = input()
+print("Привет,", name, end = "!")
